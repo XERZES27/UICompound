c9972dc3d5a4929248021158ac09bc51d4d30af4
XERZES27 <39665735+XERZES27@users.noreply.github.com>
2024-05-25 19:42:35 +0300

    connect anvil to ether

--- UICompound/src/App.tsx
+++ UICompound/src/App.tsx
@@ -1,33 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {ethers} from 'ethers';
+
 import './App.css'
+import { useEffect, useState } from 'react';
+import {contractAbi} from './data/abi.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const mainAddress:string = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+  const contractAddress:string = '0xFd822abecA70BB5560E22CEc21738bB3db802A94';
+  const [provider, setProvider] = useState<ethers.JsonRpcProvider|null>(null);
+  const [balance,setBalance] = useState<bigint|null>(null);
+  const [contract,setContract] = useState<ethers.Contract|null>(null);
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    const _provider = new ethers.JsonRpcProvider('http://localhost:8545');
+    const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', _provider);
+    // _provider.getSigner().then((signer:ethers.JsonRpcSigner)=>{
+      const _contract = new ethers.Contract(contractAddress, contractAbi.abi, wallet);
+    setContract(()=>_contract);
 
+    // });
+    
+    setProvider(()=>_provider);
+   
+  },[]);
+  
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1>Ether.js</h1>
+       <div className='balance'>
+       {provider && <button
+        onClick={async () => {
+          const _balance = await provider!.getBalance(mainAddress);
+          setBalance(_balance);
+          }}
+        >Get Balance</button>}
+        {balance && <h2>Balance of current account: {balance?.toString()}</h2>}
+       </div>
+
+
+       {
+        contract &&
+        <div className='supply'>
+        <button
+        
+          onClick={async()=>{
+          //   const payment = {
+          //     value: ethers.parseEther('1'),
+          //     gasLimit: 3000000, 
+          // };
+            // await contract.supplyCollateral(payment);
+              
+            const value = await contract.getBaseToken();
+            
+            console.log('collateralized asset value: '+ value.toString());
+
+          }}
+        >
+          Supply COMP
+          
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
+       </div>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
