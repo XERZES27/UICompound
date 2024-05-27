ae3bd02a71435a886ac5857d258dd98ef1b7bad2
XERZES27 <39665735+XERZES27@users.noreply.github.com>
2024-05-27 21:29:59 +0300

    change abi

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {contractAbi} from './data/abi.jsx';
 
 function App() {
   const mainAddress:string = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
-  const contractAddress:string = '0xfd822abeca70bb5560e22cec21738bb3db802a94';
+  const contractAddress:string = '0x9f21D9a53FC62014829c018A2E7a480364e7F02D';
   const [provider, setProvider] = useState<ethers.JsonRpcProvider|null>(null);
   const [balance,setBalance] = useState<bigint|null>(null);
   const [collateral,setCollateral] = useState<bigint|null>(null);
@@ -15,6 +15,7 @@ function App() {
   useEffect(() => {
     const _provider = new ethers.JsonRpcProvider('http://localhost:8545');
     const wallet = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', _provider);
+    
     // _provider.getSigner().then((signer:ethers.JsonRpcSigner)=>{
       const _contract = new ethers.Contract(contractAddress, contractAbi.abi, wallet);
     setContract(()=>_contract);
@@ -55,13 +56,16 @@ function App() {
             
 
 
-            const value = ethers.parseEther('1');
+            const value = ethers.parseEther('2');
             const tx = await contract.supplyCollateral({ value: value,gasLimit:10000000 });
+            console.log(tx);
             const receipt = await tx.wait();
             console.log('Transaction hash:', receipt.hash);
             
-            const collateralBalance = await contract.getValueOfAllCollateralizedAssetsE8({gasLimit:1000000 });
-            setCollateral(collateralBalance);
+            const collateralBalance = await contract.getValueOfAllCollateralizedAssetsE8();
+            
+            setCollateral(ethers.toBigInt(collateralBalance.data));
+            console.log(collateralBalance);
             
 
 
@@ -71,6 +75,19 @@ function App() {
           
         </button>
         {collateral && <h2>Collateral of current account: {collateral.toString()}</h2>}
+        <button
+        
+          onClick={async()=>{
+            const collateralBalance = await contract.getCollateralizedAssets();
+            // setCollateral(ethers.toBigInt(collateralBalance.data));
+            console.log(collateralBalance);
+            
+          }}
+        >
+          GetCOMPBalance
+          
+        </button>
+        {collateral && <h2>Value of all collateralized assets in e8: {collateral.toString()}</h2>}
         
        </div>}
       </div>
